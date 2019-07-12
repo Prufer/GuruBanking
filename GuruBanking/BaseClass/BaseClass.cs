68242a1b643995a1fab15d7aadc285ad3dd6a0a0
Sanith Kobewatta <sanithk3@gmail.com>
2019-07-12 15:48:37 +1000

    Add project files.

---
+++ GuruBanking/BaseClass/BaseClass.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuruBanking.BaseClass
+{
+   public  class BaseClassTest
+    {
+
+        public IWebDriver driver;
+        [OneTimeSetUp]
+
+        public void Open()
+        {
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.demo.guru99.com/V4";
+
+
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+
+    }
+}
---
+++ GuruBanking/TestCases/BankingUAT.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Compatibility;
+using NUnit.Framework;
+using GuruBanking.BaseClass;
+using OpenQA.Selenium.Support.UI;
+
+namespace GuruBanking.TestCases
+{
+    public class BankingUAT : BaseClassTest
+    {
+
+        [Test]
+        public void Login()
+        {
+
+            IWebElement UserID = driver.FindElement(By.XPath("//input[@name='uid']"));
+            UserID.SendKeys("mngr209033");
+
+            IWebElement Password = driver.FindElement(By.XPath("//input[@name='password']"));
+            Password.SendKeys("vehyjYn");
+
+            IWebElement LoginButton = driver.FindElement(By.XPath("//input[@name='btnLogin']"));
+            LoginButton.Click();
+
+            try
+            {
+                String URL = driver.Url;
+                Assert.AreEqual(URL, "http://www.demo.guru99.com/V4/manager/Managerhomepage.php");
+                System.Diagnostics.Debug.WriteLine("Logged in OK");
+
+            }
+
+            catch {
+
+                Console.WriteLine("Logged in FAILED");
+            }
+
+        }
+
+
+
+    }
+}
